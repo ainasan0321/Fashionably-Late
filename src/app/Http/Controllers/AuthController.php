8f2322f06d8f63791566d9520a29cc57ec6e661a
nakamura aina <Ai0321na@gmail.com>
2026-05-23 14:30:15 +0900

    確認テスト一部修正

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -48,4 +48,14 @@ class AuthController extends Controller
         ->withInput($request->only('email'));
     }
 
+        public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        
+        return redirect('/login');
+    }
+
 }
